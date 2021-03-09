f384011d03d4c0515043839bec3b3477f4efe447
trolio <42102227+trolio@users.noreply.github.com>
2021-03-08 21:06:51 -0500

    added pawn script

--- addons/locales/en.lua
+++ addons/locales/en.lua
@@ -678,4 +678,11 @@ Locales ['en'] = {
 	['highend'] = 'high end',
 	--optional needs--
 	['used_beer'] = 'you used 1x ~y~Beer~s~',
+	--pawn--
+	['dealer_prompt'] = 'press ~INPUT_CONTEXT~ to talk with the ~r~Pawn dealer~s~.',
+	['dealer_title'] = 'Pawn Dealer',
+	['dealer_item'] = '$%s',
+	['dealer_notenough'] = 'you don\'t have enough of that to sell!',
+	['dealer_sold'] = 'you\'ve sold ~b~%sx~s~ ~y~%s~s~ for ~g~$%s~s~',
+	['item'] = '$%s',
 }
